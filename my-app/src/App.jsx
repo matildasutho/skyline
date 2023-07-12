947538c07809d797aa51c7bccb25ca34cfd78cf3
Matilda Sutho <matildasutherland23@gmail.com>
2023-07-12 14:11:07 +0200

    video embed on curvedplane geometry. autplay, loop.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import React, { Suspense, useEffect, useRef } from "react";
+import React, { Suspense, useEffect, useRef, useState, useMemo } from "react";
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import {
@@ -10,49 +10,80 @@ import {
   useVideoTexture,
   useAspect,
   useCubeTexture,
+  useTexture,
 } from "@react-three/drei";
-import video from "/WZRD-skyline-test.mp4";
+import src from "/WZRD-skyline-test.mp4";
+import CurvedPlane from "./CurvedPlane.jsx";
 
 function ThreeScene() {
   const { environment } = useEnvironment();
   const { videoTexture } = useVideoTexture("/WZRD-skyline-test.mp4"); //create function to load and play video as texture?? import?
   const videoRef = useRef();
 
-  // useEffect(() => {
-  //   if (videoRef.current) {
-  //     videoRef.current.play();
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.stop();
+    }
+  }, []);
+
+  const [video, setVideo] = useState();
+
+  const ratio = 16 / 9;
+  const width = 50;
+  const radius = 4;
+  const z = 4;
+
+  const r = useMemo(
+    () => (video ? video.videoWidth / video.videoHeight : ratio),
+    [video, ratio]
+  );
 
   return (
     <>
       <OrbitControls />
       <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
 
-      <mesh>
+      {/* <mesh>
         <boxGeometry args={[200, 200, 200]} />
         <meshStandardMaterial map={videoTexture} side={THREE.DoubleSide} />
-      </mesh>
-      <Suspense fallback={<FallbackMaterial url='/ny.png' />}>
-        <mesh>
-          <planeGeometry args={[45, 25]} />
-          <VideoMaterial url={video} side={THREE.DoubleSide} />
-        </mesh>
-      </Suspense>
+      </mesh> */}
+      <CurvedPlane width={width} height={width / r} radius={radius}>
+        <Suspense
+          fallback={<meshStandardMaterial side={THREE.DoubleSide} wireframe />}
+        >
+          <VideoMaterial src={src} setVideo={setVideo} />
+        </Suspense>
+      </CurvedPlane>
       {environment && <primitive object={environment.scene} />}
-
-      <Environment
-        background
-        files={["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"]}
-        path='/'
-      />
+      <mesh>
+        <Environment
+          background
+          files={["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"]}
+          path='/'
+        />
+      </mesh>
     </>
   );
 }
 
-function VideoMaterial({ url }) {
-  const texture = useVideoTexture(url);
-  return <meshBasicMaterial map={texture} toneMapped={false} />;
+function VideoMaterial({ src, setVideo }) {
+  const texture = useVideoTexture(src);
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+  texture.repeat.x = -1;
+  texture.offset.x = 1;
+
+  setVideo?.(texture.image);
+
+  return (
+    <meshStandardMaterial
+      side={THREE.DoubleSide}
+      map={texture}
+      toneMapped={false}
+      transparent
+      opacity={0.9}
+    />
+  );
 }
 
 function FallbackMaterial({ url }) {

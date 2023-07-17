2b67050e966b86106e9801b5a65eb9c125862887
Matilda Sutho <matildasutherland23@gmail.com>
2023-07-17 16:43:51 +0200

    postprocessing of scene

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,6 +3,13 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { VRButton, XR, Controllers } from "@react-three/xr";
+import {
+  EffectComposer,
+  DepthOfField,
+  Bloom,
+  Noise,
+  Vignette,
+} from "@react-three/postprocessing";
 import "./App.css";
 import Space from "./components/Space.jsx";
 import Model from "./components/Model.jsx";
@@ -22,12 +29,38 @@ function App() {
   return (
     <>
       <VRButton />
-      <Canvas>
+      <Canvas
+        colorManagement
+        camera={{ position: [0, 0, 3] }}
+        gl={{
+          powerPreference: "high-performance",
+          alpha: false,
+          antialias: false,
+          stencil: false,
+          depth: false,
+        }}
+      >
         <XR>
           <Controllers />
           <Suspense fallback={null}>
             <ThreeScene />
           </Suspense>
+          <EffectComposer multisampling={0} disableNormalPass={true}>
+            <DepthOfField
+              focusDistance={0}
+              focalLength={0.02}
+              bokehScale={2}
+              height={480}
+            />
+            <Bloom
+              luminanceThreshold={0}
+              luminanceSmoothing={0.9}
+              height={300}
+              opacity={3}
+            />
+            <Noise opacity={0.025} />
+            <Vignette eskil={false} offset={0.1} darkness={1.1} />
+          </EffectComposer>
         </XR>
       </Canvas>
     </>
--- my-app/src/components/Space.jsx
+++ my-app/src/components/Space.jsx
@@ -11,50 +11,53 @@ import {
 import src from "/360-VR-video.mp4";
 
 export default function Space() {
-  const { environment } = useEnvironment();
-  const { videoTexture } = useVideoTexture("/WZRD-skyline-test.mp4"); //create function to load and play video as texture?? import?
-  const videoRef = useRef();
+  // const { environment } = useEnvironment();
+  // const { videoTexture } = useVideoTexture("/WZRD-skyline-test.mp4"); //create function to load and play video as texture?? import?
+  // const videoRef = useRef();
 
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.stop();
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (videoRef.current) {
+  //     videoRef.current.stop();
+  //   }
+  // }, []);
 
-  const [video, setVideo] = useState();
+  // const [video, setVideo] = useState();
 
-  const ratio = 16 / 9;
-  const width = 200;
-  const radius = 200;
-  const z = 4;
+  // const ratio = 16 / 9;
+  // const width = 200;
+  // const radius = 200;
+  // const z = 4;
 
-  const r = useMemo(
-    () => (video ? video.videoWidth / video.videoHeight : ratio),
-    [video, ratio]
-  );
+  // const r = useMemo(
+  //   () => (video ? video.videoWidth / video.videoHeight : ratio),
+  //   [video, ratio]
+  // );
+
+  //mesh distortion material
   const bumpMap = useTexture("/nx.png");
   const envMap = useCubeTexture(
     ["px.png", "nx.png", "py.png", "ny.png", "pz.png", "nz.png"],
     { path: "/" }
   );
+
   const [material, set] = useState();
   return (
     <>
       <mesh>
-        <sphereGeometry args={[50, 50, 50]} />
+        <sphereGeometry args={[25, 25, 25]} />
         <MeshDistortMaterial
           side={THREE.BackSide}
           ref={set}
           envMap={envMap}
           bumpMap={bumpMap}
-          color={"#82f74f"}
+          color={"#f6f6f6"}
           roughness={0.1}
           metalness={1}
-          bumpScale={0.005}
+          bumpScale={0.2}
           clearcoat={1}
           clearcoatRoughness={1}
           radius={1}
-          distort={0.4}
+          distort={1}
         />
         {/* <VideoMaterial src={src} setVideo={setVideo} /> */}
       </mesh>
@@ -66,27 +69,27 @@ export default function Space() {
   );
 }
 
-function VideoMaterial({ src, setVideo }) {
-  const texture = useVideoTexture(src);
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat.x = -1;
-  texture.offset.x = 1;
+// function VideoMaterial({ src, setVideo }) {
+//   const texture = useVideoTexture(src);
+//   texture.wrapS = THREE.RepeatWrapping;
+//   texture.wrapT = THREE.RepeatWrapping;
+//   texture.repeat.x = -1;
+//   texture.offset.x = 1;
 
-  setVideo?.(texture.image);
+//   setVideo?.(texture.image);
 
-  return (
-    <meshStandardMaterial
-      side={THREE.DoubleSide}
-      map={texture}
-      toneMapped={false}
-      transparent
-      opacity={0.9}
-    />
-  );
-}
+//   return (
+//     <meshStandardMaterial
+//       side={THREE.DoubleSide}
+//       map={texture}
+//       toneMapped={false}
+//       transparent
+//       opacity={0.9}
+//     />
+//   );
+// }
 
-function FallbackMaterial({ url }) {
-  const texture = useTexture(url);
-  return <meshBasicMaterial map={texture} toneMapped={false} />;
-}
+// function FallbackMaterial({ url }) {
+//   const texture = useTexture(url);
+//   return <meshBasicMaterial map={texture} toneMapped={false} />;
+// }
